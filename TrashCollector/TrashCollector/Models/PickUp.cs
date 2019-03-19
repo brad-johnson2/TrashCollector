f95cd4ce7b3096449489f750d4ad1ca3baf713a5
brad-johnson2 <46530051+brad-johnson2@users.noreply.github.com>
2019-03-19 15:19:30 -0500

    Built PickUp Class

---
+++ TrashCollector/TrashCollector/Models/PickUp.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TrashCollector.Models
+{
+    public class PickUp
+    {
+
+        [Key]
+
+        public int Id { get; set; }
+
+        public string WeekDay { get; set; }
+
+        
+
+    }
+}
